3b6559e40afc22a70516f3eb138a22dcc4d1a327
PavelSnegiryov <pasha.snegiryov@gmail.com>
2021-06-14 03:58:32 +0300

    added favicon

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 let city = 'Kyiv';
 function fetchWeather() {
-    let api = `http://api.openweathermap.org/data/2.5/weather?q=${city},ua&appid=df97a75ef89a0ef75a6abceeacf0b523`
+    let api = `http://api.openweathermap.org/data/2.5/weather?q=${city},ua&appid=df97a75ef89a0ef75a6abceeacf0b523`;
     fetch(api)
-        .then(function (resp) { return resp.json() })
+        .then(function (resp) { return resp.json(); })
         .then(function (data) {
             console.log(data);
             document.querySelector('.package-name').textContent = data.name;
@@ -11,11 +11,11 @@ function fetchWeather() {
             document.querySelector('.disclaimer').textContent = data.weather[0]['description'];
             document.querySelector('.features li').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
             document.querySelector('.wind').textContent = `Wind - ${data.wind.speed} m/s`;
-        })
+        });
 }
 document.querySelector('.b-1').onclick = () => {
     let inputCity = document.querySelector('.newCity').value;
     city = inputCity;
-    fetchWeather()
-}
-fetchWeather()
+    fetchWeather();
+};
+fetchWeather();
